bca1f0d2adb8b720dff84c113c85e79ef813bcdb
AlexTheTrashGolem <a.belorusskij@gmail.com>
2022-06-21 23:26:27 +0300

    fixing dependencies

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import server from "./server";
 
 (async () => {
   //await server.initLocalDatabases();
-  await server.initServer("3000", "0.0.0.0");
+  await server.initServer((process.env.PORT || "3000"), "0.0.0.0");
 })();
