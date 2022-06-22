2da42560f2beda6681c920d048879fd706fb4a22
AlexTheTrashGolem <a.belorusskij@gmail.com>
2022-06-22 10:19:38 +0300

    fixing animeTypes.ts

--- src/modules/animeModule/types/animeTypes.ts
+++ src/modules/animeModule/types/animeTypes.ts
@@ -1,2 +1,2 @@
 export type genre = "action" | "comedy" | "fantasy" | "romance" | "slice_of_life"
-export type status = "completed" | "in_progress" | "dropped" | "plan_to_watch"
+export type status = "finished" | "airing" | "anonsed"
