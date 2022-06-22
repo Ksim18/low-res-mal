75f3e2bd10b0c77692889075db3c61e250b1364f
AlexTheTrashGolem <a.belorusskij@gmail.com>
2022-06-22 12:06:19 +0300

    fixing update query for animeList

--- src/utils/crudHelper.ts
+++ src/utils/crudHelper.ts
@@ -58,8 +58,8 @@ export function constructUpdateQueryStringBasedOnParams(
     updateRecordQueryString += ` WHERE $${counter} = `;
     columnValues.push(data.searchBy[i]);
     counter++;
-    updateRecordQueryString += `'$${counter}' and`;
-    columnValues.push(data.value[i]);
+    updateRecordQueryString += `$${counter} and`;
+    columnValues.push(data.value[i].toString);
     counter++;
   }
   updateRecordQueryString = updateRecordQueryString.substring(
