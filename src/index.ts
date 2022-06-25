255f40d26c2468204030f4f3bc5c861dbc9d8fa0
AlexTheTrashGolem <a.belorusskij@gmail.com>
2022-06-25 17:44:52 +0300

    fixing environment options

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import server from "./server";
 
 (async () => {
   //await server.initLocalDatabases();
-  await server.initServer((process.env.PORT || "3000"), <string>process.env.DB_HOST);
+  await server.initServer((process.env.PORT || "3000"), (<string>process.env.HOST || "0.0.0.0"));
 })();
