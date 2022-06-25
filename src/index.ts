833d553158756d8153c637bdf531729700384042
AlexTheTrashGolem <a.belorusskij@gmail.com>
2022-06-25 17:56:44 +0300

    fixing environment options

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import server from "./server";
 
 (async () => {
   //await server.initLocalDatabases();
-  await server.initServer((process.env.PORT || "3000"), (<string>process.env.HOST || "0.0.0.0"));
+  await server.initServer((process.env.PORT || "3000"), "0.0.0.0");
 })();
